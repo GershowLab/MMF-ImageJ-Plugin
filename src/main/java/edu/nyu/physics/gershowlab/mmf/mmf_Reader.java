199d56e494cd4e236d1de1300b45f51d032361da
Natalie Bernat <Natalie.Bernat@Gmail.com>
2013-12-06 15:07:05 -0500

    Added run() code to convert MMF to VirtualStack; Added methods (makeVirtStackFromMMF and getProcessor) to implement

--- src/main/java/edu/nyu/physics/gershowlab/mmf/mmf_Reader.java
+++ src/main/java/edu/nyu/physics/gershowlab/mmf/mmf_Reader.java
@@ -1,8 +1,23 @@
 /* mmf_Reader
- * reads mmf files into imagej
- * will eventually have a better header
  * 
  * (c) 2013 Natalie Bernat and Marc Gershow
+
+ *  File reader for the MMF movie format through ImageJ.
+ *  
+ *  
+ *   Opens a dialog to choose an MMF. Opens the specified file as an MmfFile, 
+ *   and reads the image frames into a VirtualStack. The virtual stack is used to 
+ *   create an ImagePlus, which is opened by ImageJ. 
+ *   
+ *  
+ *  ~Update 12/6/13~
+ *  -Wrote a descriptive header
+ *  -Reader now extends VirtualStack
+ *  -Added run() code to convert file to Virtual Stack
+ *  -Added unimplemented makeVirtStackFromMMF() and getProcessor() methods
+ *  
+ *  
+ *  
  *  
  */
 package edu.nyu.physics.gershowlab.mmf;
@@ -18,27 +33,21 @@ import ij.gui.*;
 import ij.plugin.*;
 import ij.plugin.frame.*;
 
-public class mmf_Reader implements PlugIn {
+public class mmf_Reader extends VirtualStack implements PlugIn {
 
 	//Used to print debugging messages 
 	//  Multiple numbers can be used for different kinds/levels of debug feedback
 	private int debug = 1;
 	
+	private String			path;
+	private String			fileName;
+	private String			fileDir;
+	private MmfFile 		raf;					//the mmf file
+	private	ImageStack		vStack;
+	private	ImagePlus		imp;
 	
-	
-	
-	
-	private MmfFile raf;					//the mmf file
-	
-	
+		
 	private CommonBackgroundStack currentStack;
-	
-	public ImageProcessor getImage (int frameNumber) {
-		//check if current stack has frame
-		//if not update current stack from mmf file
-		//then get specific frame
-	}
-
 	
 	
 	/////////////////////////////
@@ -47,7 +56,7 @@ public class mmf_Reader implements PlugIn {
 	
 	public void run(String arg) {
 		
-		String path = getPath(arg);
+		getPath(arg);
 		if (null == path) {
 			//This is used when the dialog is cancelled
 			return;
@@ -61,16 +70,67 @@ public class mmf_Reader implements PlugIn {
 			return;
 		}
 		
+		//Create the Virtual Stack
+		try{	
+			//TODO put correct arguments when this is written; also correct the variables two blocks down
+			vStack = makeStackFromMMF(path, firstFrame, lastFrame, isVirtual, convertToGray, flipVertical);
+		} catch(Exception e){
+			IJ.showMessage("mmfReader","Error on creation of Virtual Stack.\n\n Error: " +e);
+			return;
+		}
 		
-		//make the reference table 
+		//Check that the vStack was made
+		if (vStack==null || (vStack.isVirtual()&&vStack.getProcessor(1)==null))
+			return;
 		
+		//If the file is empty, provide some informative notes:
+		if (vStack.getSize() == 0) {
+			String rangeText = "";
+			if (firstFrame>1 || lastFrame!=0)
+				rangeText = "\nin Range "+firstFrame+
+					(lastFrame>0 ? " - "+lastFrame : " - end");
+			error("Error: No Frames Found"+rangeText);
+			return;
+		}
+		
+		//Make the ImagePlus from the stack, then add the FileInfo
+		imp = new ImagePlus(WindowManager.makeUniqueName(fileName), vStack);
+		/*
+		if (imp.getBitDepth()==16)
+			imp.getProcessor().resetMinAndMax();
+		setFramesPerSecond(imp);
+		*/
+		FileInfo fi = new FileInfo();
+		fi.fileName = fileName;
+		fi.directory = fileDir;
+		imp.setFileInfo(fi);
+		
+		
+		//Play the movie
+		imp.show("Playing MMF: "+ fileName);
+		
+		
+		//make the reference table (done in MmfFile: parse() ) 
 		//read each stack
+		
+		
+		//Take stack and convert it to imageProcessor
+		
 		
 		//if (debug>0){
 	//		IJ.showMessage("mmfReader","Path Name:\n"+path);
 	//	}
 		
 	}
+	
+	//Creates a Virtual Stack using the MmfFile
+	private ImageStack makeVirtStackFromMMF(path, firstFrame, lastFrame, isVirtual, convertToGray, flipVertical){
+		//TODO
+		//See AVI_Reader: makeStack & readAVI
+	}
+	
+	
+	
 	
 	//Checks that the file exists, or opens a dialog for the user to choose a file. 
 	//  Returns the path name
@@ -88,8 +148,29 @@ public class mmf_Reader implements PlugIn {
 		if (null == dir) return null; // dialog was canceled
 		dir = dir.replace('\\', '/'); // Windows safe
 		if (!dir.endsWith("/")) dir += "/";
-		return dir + od.getFileName();
+		fileName = od.getFileName();
+		fileDir = dir;
+		path = fileDir + fileName;
+		return path;
 	}
+	
+	//Returns the ImageProcessor for the specified frame number
+	//	Overrides the method in ImageStack
+	//	Ensures that the frame is in the current mmfStack, and then 
+	public ImageProcessor getProcessor (int frameNumber) {
+		//TODO
+		//check if current stack has frame
+		//if not update current stack from mmf file
+		if(frameNumber<currentStack.getFirstFrame() || frameNumber>currentStack.getLastFrame()){
+			currentStack = raf.getStackForFrame(frameNumber);
+		}
+		//then get specific frame
+		return currentStack.getImage(frameNumber);
+		
+		
+	}
+	
+	
 	
 	//Main file reader:
 	//		~Processes header
@@ -105,7 +186,7 @@ public class mmf_Reader implements PlugIn {
         // start ImageJ
         new ImageJ();
 
-        // open the Clown sample
+       // open the (incredibly creepy) Clown sample
        // ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
        // image.show();
 
